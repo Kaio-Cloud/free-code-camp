c5c98817a59826cc375cfccd257ff1013b85707e
Kaio-Could <Kaio.phelipe12@live.com>
2021-04-24 15:25:16 -0300

    setima aula

---
+++ javascript-algorithms-and-data-structures/basic-javascript/understanding-case-sensitivity-in-variables.js
@@ -0,0 +1,9 @@
+// declarando as variaveis 
+var studlyCapVar = 10;
+var properCamelCase = "A string";
+var titleCaseOver = 9000;
+
+// verificando as atribuições mencionandas anteriormente 
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
